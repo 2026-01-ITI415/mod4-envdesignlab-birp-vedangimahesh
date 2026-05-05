0210504c121b1f47f9e072b5cbf6e3f7838ae80e
vedangimahesh <113918384+vedangimahesh@users.noreply.github.com>
2026-05-05 17:13:55 -0400

    height

--- Assets/Simplemovements.cs
+++ Assets/Simplemovements.cs
@@ -1,15 +1,40 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class SimpleMove : MonoBehaviour
 {
     public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpForce = 2f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.forward * v + transform.right * h;
-        transform.position += move * speed * Time.deltaTime;
+        Vector3 move = transform.right * h + transform.forward * v;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; 
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
     }
 }
